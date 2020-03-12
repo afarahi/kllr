f0d7d8d491c48060118af7f65c89699e2ef25c73
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2020-03-11 22:36:49 -0400

    Changed function names

--- kllr/__init__.py
+++ kllr/__init__.py
@@ -1,4 +1,4 @@
 from .regression_model import kllr_model
-from .regression_plotting import Plot_Fit, Plot_Fit_Split, Plot_Fit_Params, Plot_Fit_Params_Split,\
-                Plot_Cov_Corr, Plot_Cov_Corr_Split, Plot_Cov_Corr_Matrix, Plot_Cov_Corr_Matrix_Split,\
+from .regression_plotting import Plot_Fit, Plot_Fit_Split, Plot_Fit_Params, Plot_Fit_Params_Split, \
+                Plot_Cov_Corr, Plot_Cov_Corr_Split, Plot_Cov_Corr_Matrix, Plot_Cov_Corr_Matrix_Split, \
                 Plot_Residual, Plot_Residual_Split
