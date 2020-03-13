ec97849af72b37e9ff6f7f71a85d384ddf42c134
afarahi <aryaf66@gmail.com>
2020-03-13 11:29:48 -0400

    a bug fixed (calc residuals) and xlog, ylog added

--- example.py
+++ example.py
@@ -20,7 +20,7 @@ y = np.array(df.MGas)
 # data, ax = Plot_Fit(df, 'M200', 'MStar', show_data=True, cutoff=13.5, xlog=True, ylog=False, ax=ax) ## PASSED
 # data, ax = Plot_Fit_Split(df, 'M200', 'MStar_BCG100', 'z_form',  xlog=True, ylog=False, split_bins=3) ## PASSED
 # data, ax = Plot_Fit_Params(df, 'M200', 'MStar_BCG100', xlog=False) ## PASSED
-data, ax = Plot_Fit_Params_Split(df, 'M200', 'MStar_BCG100', 'z_form', split_bins = 4, xlog=True, split_mode='Residuals') # split_bins = [0.0, 0.2, 0.3, 0.6]
+# data, ax = Plot_Fit_Params_Split(df, 'M200', 'MStar_BCG100', 'z_form', split_bins = 4, xlog=True, split_mode='Residuals') # split_bins = [0.0, 0.2, 0.3, 0.6]
 # data, ax = Plot_Cov_Corr(df, 'M200', 'MGas', 'MStar', Output_mode = 'corr') ## PASSED
 # data, ax = Plot_Cov_Corr_Split(df, 'M200', 'MGas', 'MStar_BCG100', 'z_form', split_bins = 3, Output_mode = 'corr') ## DID NOT PASS, strang results
 #### ax = Plot_Correlation_Matrix(df, 'M200', ['MGas', 'MGas_T', 'sigma_DM_3D']) ## PASSED
