bb959cc3f657794c457f18fd024a4bc9c9e51270
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2021-08-23 11:49:06 -0500

    Fixed Bugs

--- kllr/__init__.py
+++ kllr/__init__.py
@@ -1,4 +1,6 @@
-from .regression_model import kllr_model
-from .regression_plotting import Plot_Fit, Plot_Fit_Split, Plot_Fit_Params, Plot_Fit_Params_Split, \
-                Plot_Cov_Corr_Matrix, Plot_Cov_Corr_Matrix_Split, \
-                Plot_Residual, Plot_Residual_Split, Plot_Skewness, Plot_Skewness_Split
+from .regression_model import kllr_model, calculate_weights
+from .regression_plotting import Plot_Fit_Summary, Plot_Fit_Summary_Split,\
+                                 Plot_Cov_Corr_Matrix, Plot_Cov_Corr_Matrix_Split, \
+                                 Plot_Residual, Plot_Residual_Split, \
+                                 Plot_Higher_Moments, Plot_Higher_Moments_Split,\
+                                 set_params, get_params
